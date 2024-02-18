f0c49ad71b7d75381a048343050505e7f9834185
jksjose <144999599+jksjose@users.noreply.github.com>
2024-02-18 22:21:22 +0800

    Finish the code of rock, paper, scissor game

--- script_loop.js
+++ script_loop.js
@@ -14,48 +14,60 @@ function getComputerChoice() {
 }
 
 function playGame() {
+  let userScore = 0;
+  let computerScore = 0;
+
   for (let i = 0; i < 5; i++) {
     let userChoice = prompt("Enter your choice");
     let userSelection = userChoice.slice(0, 1).toUpperCase() + userChoice.slice(1).toLocaleLowerCase();
     let computerSelection = getComputerChoice();
-    let userScore = 0;
-    let computerScore = 0;
+
+    if (userSelection !== "Rock" &&
+        userSelection !== "Paper" &&
+        userSelection !== "Scissor") {
+          alert("Invalid choice");
+        }
 
     function playRound(userSelection, computerSelection) {
       let roundResult;
-      let roundScore = 0;
+      let userRoundScore = 0;
+      let computerRoundScore = 0;
 
       if (userSelection === "Rock" && computerSelection === "Scissor") {
         roundResult = "You won! Rock beats scissor";
-        roundScore = 1
+        userRoundScore = 1
       } else if (userSelection === "Paper" && computerSelection === "Rock") {
         roundResult = "You won! Paper beats rock";
-        roundScore = 1
+        userRoundScore = 1
       } else if (userSelection === "Scissor" && computerSelection === "Paper") {
         roundResult = "You won! Scissor beats paper";
-        roundScore = 1
+        userRoundScore = 1
       } else if (userSelection === "Scissor" && computerSelection === "Rock") {
         roundResult = "You lose! Rock beats scissor";
-        roundScore = 1
+        computerRoundScore = 1
       } else if (userSelection === "Rock" && computerSelection === "Paper") {
         roundResult = "You lose! Paper beats rock";
-        roundScore = 1
+        computerRoundScore = 1
       } else if (userSelection === "Paper" && computerSelection === "Scissor") {
         roundResult = "You lose! Scissor beats paper";
-        roundScore = 1
-      } else {
+        computerRoundScore = 1
+      } else if (userSelection === "Rock" && computerSelection === "Rock" ||
+                 userSelection === "Paper" && computerSelection === "Paper" ||
+                 userSelection === "Scissor" && computerSelection === "Scissor") {
         roundResult = "It's a draw";
+      } else {
+        roundResult = "User has invalid choice"
       }
 
       console.log(roundResult);
-      userScore = roundScore;
-      computerScore = roundScore;
-      roundScore = 0;
+      userScore += userRoundScore;
+      computerScore += computerRoundScore;
+      userRoundScore = 0;
+      computerRoundScore = 0;
     }
     playRound(userSelection, computerSelection);
-  }
+  }  
 
-  
   let gameResult = "Your score: " + userScore + " Computer score: " + computerScore;
   console.log(gameResult);
 }
