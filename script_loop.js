2a41fc62b192366c5aa36786293609ae64f9025d
jksjose <144999599+jksjose@users.noreply.github.com>
2024-02-18 17:08:17 +0800

    Create a function for computer choice

---
+++ script_loop.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  let rock = 1;
+  let paper = 2;
+  let scissor = 3;
+
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    return "Rock";
+  } else if (randomNumber === 2) {
+    return "Paper";
+  } else {
+    return "Scissor";
+  }
+}
